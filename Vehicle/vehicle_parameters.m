e6253e5fc512d9831385d4660879652eed57390c
GiacomoRipamonti <102607208+GiacomoRipamonti@users.noreply.github.com>
2022-08-10 18:42:31 +0200

    Add files via upload

--- Vehicle/vehicle_parameters.m
+++ Vehicle/vehicle_parameters.m
@@ -11,13 +11,22 @@ k_tu = 1.5e5;
 
 %body parameters
 M = 1000; %weight
+w= 1.6;%wheel distance
 a = 2;
 b = 2.5;
 h_gc = 0.15;
 Jy = 500;
 Jx = 300;
 Jz = 600; %kg*m^2
-ca = 0.5 %air resistance
+ca = 0.5; %air resistance
 ks = 40000;
 cs = sqrt(2*ks*M);
 %Jw = 80; %tire inertia
+
+
+
+L=sqrt(a^2+w^2 /4); 
+R=sqrt(b^2+w^2 /4);
+alpha=atan(w/(2*b));
+theta=atan(w/(2*a));
+
