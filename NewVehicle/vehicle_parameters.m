b997d50b628931558ad3186eb6e9e5869b065c5a
Simone Rotondi <rotondi97simone@gmai.com>
2022-08-10 15:59:04 +0200

    add vehicle example

---
+++ NewVehicle/vehicle_parameters.m
@@ -0,0 +1,22 @@
+%tyre parameters
+r=0.2;
+k_t=1.5e5; %longitudinal stiffness
+c=0.13;
+mu_st = 0.8;
+mu_dyn = 0.4;
+k_r = 1e4; %no idea
+S = 0.13*0.2;
+k_tv = 4e4;
+k_tu = 1.5e5;
+
+%body parameters
+M = 1000; %weight
+a = 2;
+b = 2.5;
+h_gc = 0.15;
+Jy = 500;
+Jx = 300;
+Jz = 600; %kg*m^2
+ca = 0.5 %air resistance
+ks = 
+%Jw = 80; %tire inertia
