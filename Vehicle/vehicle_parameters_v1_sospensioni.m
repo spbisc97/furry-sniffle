fc097a5272d673414b3ead7289e98b3972d1f7af
Simone Rotondi <rotondi97simone@gmai.com>
2022-09-07 17:38:23 +0200

    add names to vars in parameters

--- Vehicle/vehicle_parameters_v1_sospensioni.m
+++ Vehicle/vehicle_parameters_v1_sospensioni.m
@@ -1,23 +1,24 @@
 clear all
 %tyre parameters
-r=0.3;
+r=0.3;%wheel radius
 k_t=1.5e5; %longitudinal stiffness
-%c=0.2;%0.13;
-mu_st = 0.8;
-mu_dyn = 0.2;
+
+mu_st = 0.8;%static tire-road friction coefficient
+mu_dyn = 0.2;% dynamic tire-road friction coefficient
 k_r = 200*1e4;%1e4; %no idea
-%S = 0.2*c;
-k_tv = 10*40000;%400;%4e4;
-k_tu = 10*1.5e5;%1500;%1.5e5;
+k_tv = 10*40000;%Lateral tire stiffness [N/rad]
+k_tu = 10*1.5e5;%Longitudinal tire stiffness [N]
+
+%c=0.2;%0.13;
 
 %body parameters
 M = 1500; %weight
 w= 1.6;%wheel distance
-a = 1.3;%2;
-b = 1.8;%2.5
-h_gc = 0.35;
-Jy = 500;
-Jx = 300;
+a = 1.3;%distance btw CoG and front;
+b = 1.8;% distance btw CoG and rear
+h_gc = 0.35;%height CoG
+Jy = 500;%kg*m^2
+Jx = 300;%kg*m^2
 Jz = 600; %kg*m^2
 ca = 0.5; %air resistance
 ks = 39000/4;
@@ -28,13 +29,13 @@ J_wheel = 0.4;
 
 
 
-L=sqrt(a^2+w^2 /4); 
-R=sqrt(b^2+w^2 /4);
-alpha=atan(w/(2*b));
-theta=atan(w/(2*a));
+L=sqrt(a^2+w^2 /4); %distance between CoG and front wheel 
+R=sqrt(b^2+w^2 /4);%distance between CoG and rear wheel
+alpha=atan(w/(2*b));%[rad]
+theta=atan(w/(2*a));%[rad]
 
 
-%parametri motore (a caso per ora, ma altrimenti non posso far girare la simulazione)
+%engine parameters
 K_motore = 1;
 R_motore = 0.210;
 L_motore = 0.001100;
