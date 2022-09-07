bc6b29e31303ceb9d8482190cee70ad507324d08
Simone Rotondi <rotondi97simone@gmai.com>
2022-09-07 18:11:43 +0200

    remove not needed comments

--- Vehicle_public/vehicle_parameters_v1_sospensioni.m
+++ Vehicle_public/vehicle_parameters_v1_sospensioni.m
@@ -1,5 +1,5 @@
 clear all
-%tyre parameters
+%% tyre parameters
 r=0.3;%wheel radius
 k_t=1.5e5; %longitudinal stiffness
 
@@ -9,9 +9,7 @@ k_r = 200*1e4;%1e4; %no idea
 k_tv = 10*40000;%Lateral tire stiffness [N/rad]
 k_tu = 10*1.5e5;%Longitudinal tire stiffness [N]
 
-%c=0.2;%0.13;
-
-%body parameters
+%% body parameters
 M = 1500; %weight
 w= 1.6;%wheel distance
 a = 1.3;%distance btw CoG and front;
@@ -23,11 +21,7 @@ Jz = 600; %kg*m^2
 ca = 0.5; %air resistance
 ks = 39000/4;
 cs = sqrt(2*ks*M);
-%Jw = 80; %tire inertia
 J_wheel = 0.4;
-
-
-
 
 L=sqrt(a^2+w^2 /4); %distance between CoG and front wheel 
 R=sqrt(b^2+w^2 /4);%distance between CoG and rear wheel
@@ -35,7 +29,7 @@ alpha=atan(w/(2*b));%[rad]
 theta=atan(w/(2*a));%[rad]
 
 
-%engine parameters
+%% engine parameters
 K_motore = 1;
 R_motore = 0.210;
 L_motore = 0.001100;
